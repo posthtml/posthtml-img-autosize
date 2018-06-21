e3addab4c86be1ca942ea5ac3a120da7ac168f69
Patrick Eriksson <paleite@kth.se>
2018-06-21 10:48:52 +0200

    Fix tests
    Exceptions weren't thrown when images were missing

--- test/imgAutosize.js
+++ test/imgAutosize.js
@@ -4,151 +4,139 @@ const imgAutosize = require('..');
 
 describe('Plugin', () => {
     context('options.processEmptySize == false (default)', () => {
-        it('should autosize local JPG', () => {
-            return Promise.all([
-                init(
-                    '<img src="test/img/100x201.jpg" width="auto" height="auto">',
-                    '<img src="test/img/100x201.jpg" width="100" height="201">'
-                ),
+        const optionsRoot = { root: './test/img' };
 
-                init(
-                    '<img src="test/img/100x201.jpg" width="auto">',
-                    '<img src="test/img/100x201.jpg" width="100">'
-                ),
-
-                init(
-                    '<img src="test/img/100x201.jpg" height="auto">',
-                    '<img src="test/img/100x201.jpg" height="201">'
+        it('should autosize local JPG', async () => {
+            expect(
+                await init(
+                    '<img src="100x201.jpg" width="auto" height="auto">',
+                    optionsRoot
                 )
-            ]);
+            ).toBe('<img src="100x201.jpg" width="100" height="201">');
+
+            expect(
+                await init('<img src="100x201.jpg" width="auto">', optionsRoot)
+            ).toBe('<img src="100x201.jpg" width="100">');
+
+            expect(
+                await init('<img src="100x201.jpg" height="auto">', optionsRoot)
+            ).toBe('<img src="100x201.jpg" height="201">');
         });
 
-
-        it('should autosize local PNG', () => {
-            return init(
-                '<img src="img/111x52.png" width="auto" height="104" alt="hi">',
-                '<img src="img/111x52.png" width="222" height="104" alt="hi">',
-                {root: './test'}
-            );
+        it('should autosize local PNG', async () => {
+            expect(
+                await init(
+                    '<img src="111x52.png" width="auto" height="104" alt="hi">',
+                    optionsRoot
+                )
+            ).toBe('<img src="111x52.png" width="222" height="104" alt="hi">');
         });
 
-
-        it('should autosize remote PNG', () => {
-            return init(
-                '<img src="http://placehold.it/200x200" width="auto" height="25" alt="hi">',
+        it('should autosize remote PNG', async () => {
+            expect(
+                await init(
+                    '<img src="http://placehold.it/200x200" width="auto" height="25" alt="hi">',
+                    optionsRoot
+                )
+            ).toBe(
                 '<img src="http://placehold.it/200x200" width="25" height="25" alt="hi">'
             );
         });
 
-
-        it('should autosize local GIF', () => {
-            return init(
-                '<img src="140x83.gif" width="auto" height="auto">',
-                '<img src="140x83.gif" width="140" height="83">',
-                {root: './test/img'}
-            );
+        it('should autosize local GIF', async () => {
+            expect(
+                await init(
+                    '<img src="140x83.gif" width="auto" height="auto">',
+                    optionsRoot
+                )
+            ).toBe('<img src="140x83.gif" width="140" height="83">');
         });
 
-
-        it('should autosize local BMP', () => {
-            return init(
-                '<img src="33x16.bmp" width="auto" height="auto">',
-                '<img src="33x16.bmp" width="33" height="16">',
-                {root: './test/img'}
-            );
+        it('should autosize local BMP', async () => {
+            expect(
+                await init(
+                    '<img src="33x16.bmp" width="auto" height="auto">',
+                    optionsRoot
+                )
+            ).toBe('<img src="33x16.bmp" width="33" height="16">');
         });
 
-
-        it('should autosize local WebP', () => {
-            return init(
-                '<img src="163x53.webp" width="auto" height="auto">',
-                '<img src="163x53.webp" width="163" height="53">',
-                {root: './test/img'}
-            );
+        it('should autosize local WebP', async () => {
+            expect(
+                await init(
+                    '<img src="163x53.webp" width="auto" height="auto">',
+                    optionsRoot
+                )
+            ).toBe('<img src="163x53.webp" width="163" height="53">');
         });
 
-
-        it('should autosize local TIFF', () => {
-            return init(
-                '<img src="63x69.tiff" width="auto" height="auto">',
-                '<img src="63x69.tiff" width="63" height="69">',
-                {root: './test/img'}
-            );
+        it('should autosize local TIFF', async () => {
+            expect(
+                await init(
+                    '<img src="63x69.tiff" width="auto" height="auto">',
+                    optionsRoot
+                )
+            ).toBe('<img src="63x69.tiff" width="63" height="69">');
         });
 
-
-        it('should autosize local SVG', () => {
-            return init(
-                '<img src="203x150.svg" width="auto" height="auto">',
-                '<img src="203x150.svg" width="203" height="150">',
-                {root: './test/img'}
-            );
+        it('should autosize local SVG', async () => {
+            expect(
+                await init(
+                    '<img src="203x150.svg" width="auto" height="auto">',
+                    optionsRoot
+                )
+            ).toBe('<img src="203x150.svg" width="203" height="150">');
         });
 
-
-        it('should skip <img> with empty "src"', () => {
+        it('should skip <img> with empty "src"', async () => {
             const html = '<div><img></div>';
-            return init(html,html);
+            expect(await init(html)).toBe(html);
         });
 
-
-        it('should skip <img> with defined "width" and "height"', () => {
-            const html = '<img src="foo.jpg" widht="100%" height="100">';
-            return init(html, html);
+        it('should skip <img> with defined "width" and "height"', async () => {
+            const html = '<img src="foo.jpg" width="100%" height="100">';
+            expect(await init(html)).toBe(html);
         });
 
-
-        it('should skip <img> with "width" and "height" != "auto"', () => {
+        it('should skip <img> with "width" and "height" != "auto"', async () => {
             const html = '<img src="foo.jpg">';
-            return init(html, html);
+            expect(await init(html)).toBe(html);
         });
 
-
-        it('should throw an error if the image is not found', () => {
+        it('should throw an error if the image is not found', async () => {
             const html = '<img src="notExists.jpg" width="auto" height="auto">';
-            return init(html, html).catch(error => {
-                expect(error.message)
-                    .toInclude('ENOENT')
-                    .toInclude('notExists.jpg');
-            });
+            let errorMessage;
+            try {
+                await init(html);
+            } catch (e) {
+                errorMessage = e.message;
+            }
+            expect(errorMessage).toContain('ENOENT');
+            expect(errorMessage).toContain('notExists.jpg');
         });
     });
 
-
-
-
     context('options.processEmptySize == true', () => {
-        it('should autosize <img> with empty "width" or "height"', () => {
-            const options = {processEmptySize: true};
+        const options = { root: './test/img', processEmptySize: true };
 
-            return Promise.all([
-                init(
-                    '<img src="test/img/100x201.jpg">',
-                    '<img src="test/img/100x201.jpg" width="100" height="201">',
-                    options
-                ),
+        it('should autosize <img> with empty "width" or "height"', async () => {
+            expect(await init('<img src="100x201.jpg">', options)).toBe(
+                '<img src="100x201.jpg" width="100" height="201">'
+            );
 
-                init(
-                    '<img src="test/img/111x52.png" width="">',
-                    '<img src="test/img/111x52.png" width="111" height="52">',
-                    options
-                ),
+            expect(await init('<img src="111x52.png" width="">', options)).toBe(
+                '<img src="111x52.png" width="111" height="52">'
+            );
 
-                init(
-                    '<img src="test/img/140x83.gif" height="auto">',
-                    '<img src="test/img/140x83.gif" height="83" width="140">',
-                    options
-                )
-            ]);
+            expect(
+                await init('<img src="140x83.gif" height="auto">', options)
+            ).toBe('<img src="140x83.gif" height="83" width="140">');
         });
     });
 });
 
-
-function init(html, expectedHtml, options) {
+function init(html, options) {
     return posthtml([imgAutosize(options)])
         .process(html)
-        .then(result => {
-            expect(result.html).toBe(expectedHtml);
-        });
+        .then(result => result.html);
 }
